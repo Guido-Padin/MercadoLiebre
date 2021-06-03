0f651254f3893572b6dc0bc0be25dbd15200577c
Guido-Padin <guidopadin17@gmail.com>
2021-06-02 23:59:40 -0300

    update

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname,"./public")));
 
-app.listen(3000, () => console.log("server start in http://localhost:3000"));
+app.set("port",process.env.PORT || 3000)
+
+app.listen(app.get("port"), () => console.log("server start in http://localhost:3000"));
 
 app.get("/",(req,res) => res.sendFile(path.resolve(__dirname,"./views/home.html")));
 
